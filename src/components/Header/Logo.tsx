eedbc0f7ff822ee159d55ac06c9706f732dc000b
Hung Nguyen <nbhung278@gmail.com>
2023-11-26 00:32:09 +0700

    first commit

---
+++ src/components/Header/Logo.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import profileImg from "@/project_files/profile-img.png";
+
+const Logo = () => {
+  return (
+    <Link href={"/"} className="flex items-center text-dark gap-4">
+      <div className="w-16 rounded-full overflow-hidden border border-solid border-dark">
+        <Image
+          src={profileImg}
+          alt="Logo"
+          className="w-full h-auto rounded-full"
+        />
+      </div>
+      <span className="font-bold text-xl">CodeBeos</span>
+    </Link>
+  );
+};
+
+export default Logo;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Logo from "./Logo";
+import Link from "next/link";
+import Image from "next/image";
+import InstagramLogo from "@/src/assets/images/instagram-logo.png";
+import MetaLogo from "@/src/assets/images/meta-logo.png";
+import GithubLogo from "@/src/assets/images/github-logo.png";
+import DribbleLogo from "@/src/assets/images/dribble-logo.png";
+import { SunIcon } from "../Icon";
+
+const Header = () => {
+  return (
+    <header className="w-full p-4 px-10 flex items-center justify-between">
+      <Logo />
+      <nav
+        className="w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize flex items-center 
+      gap-5 fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm"
+      >
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+        <button>
+          <SunIcon />
+        </button>
+      </nav>
+      <div className="flex flex-row gap-4 items-center">
+        <Link href="/">
+          <Image
+            src={InstagramLogo}
+            alt="instagram logo"
+            width={36}
+            height={36}
+            className="hover:scale-125 transition-all ease duration-200"
+          />
+        </Link>
+        <Link href="/">
+          <Image
+            src={MetaLogo}
+            alt="instagram logo"
+            width={36}
+            height={36}
+            className="hover:scale-125 transition-all ease duration-200"
+          />
+        </Link>
+        <Link href="/">
+          <Image
+            src={GithubLogo}
+            alt="Github logo"
+            width={26}
+            height={26}
+            className="hover:scale-125 transition-all ease duration-200"
+          />
+        </Link>
+        <Link href="/">
+          <Image
+            src={DribbleLogo}
+            alt="Dribble logo"
+            width={32}
+            height={32}
+            className="hover:scale-125 transition-all ease duration-200"
+          />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
